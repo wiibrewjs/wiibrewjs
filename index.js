818c4a3aadc1c8c174b6348e936e8e727e4fd45f
Karim Alibhai <karim@alibhai.co>
2016-07-02 03:42:31 -0400

    update flow to support methods

--- index.js
+++ index.js
@@ -4,13 +4,59 @@
  */
 
 const createSocket = require('./lib/socket')
+const METHODS = (function (methods) {
+    let tmp
+
+    for (let method in methods) {
+        if (methods.hasOwnProperty(method)) {
+            tmp = new Buffer(4)
+            tmp.writeInt32BE(methods[method])
+            methods[method] = tmp
+        }
+    }
+
+    return methods
+}({
+    print: 0x01
+}))
+
+const util = require('util')
 const EventEmitter = require('events').EventEmitter
 
-module.exports = ip => {
-    let socket = createSocket(ip)
-    let app = new EventEmitter()
+class App extends EventEmitter {
+    constructor () {
+        super()
 
-    // TODO: write events API between loader and library
+        this.socket = null
+    }
 
-    return app
+    init ( ip ) {
+        return new Promise((resolve, reject) => createSocket(ip).then(socket => {
+            this.socket = socket
+            resolve() 
+        }, reject))
+    }
+
+    load ( assets ) {
+        // force array
+        if (!Array.isArray( assets )) {
+            assets = [assets]
+        }
+
+        return new Promise((resolve, reject) => {
+            // ...
+        })
+    }
+
+    print () {
+        const text = util.format.apply(util, arguments)
+        const size = new Buffer(4)
+        size.writeInt32BE(text.length)
+
+        this.socket.write(METHODS.print)
+        this.socket.write(size)
+        this.socket.write(new Buffer(text))
+    }
 }
+
+module.exports = () => new App()
