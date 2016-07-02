3ad6371e019c9e4b79c26be2e3de40837100738b
Karim Alibhai <karim@alibhai.co>
2016-07-02 03:50:29 -0400

    update example

--- example/index.js
+++ example/index.js
@@ -3,12 +3,10 @@
 const app = require('../')()
 
 app.init('192.168.1.118').then(() => {
-    app.print('Hello, world.')
-
+  app.print('Hello, world.')
+  app.load([
+    app.img('bg', 'img/bg.png')
+  ]).then(() => {
     // ...
-    const background = app.image('bg', 'img/background.png')
-
-    app.load(background).then(() => {
-        // ...
-    })
+  }, console.log)
 }, console.log)
