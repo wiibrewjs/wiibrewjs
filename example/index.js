aa914c1bfae924bfc37ef0f63b4277864da7179a
Karim Alibhai <karim@alibhai.co>
2016-07-03 03:12:12 -0400

    add log statements

--- example/index.js
+++ example/index.js
@@ -2,7 +2,9 @@
 
 const app = require('../')()
 
+console.log('* Connecting ...')
 app.init('192.168.1.118').then(() => {
+  console.log('* Established connection.')
   app.print('Hello, world.')
   app.load([
     app.img('bg', 'img/bg.png')
